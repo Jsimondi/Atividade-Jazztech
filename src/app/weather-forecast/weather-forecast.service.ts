bc1123610457361b91f133176a66dfa99fa31cc3
Jsimondi <simondi.jorge@gmail.com>
2021-11-01 16:29:53 -0300

    Criados metodos GET para: Validacao da chave e Obter a temperatura de uma cidade dado o seu nome ou codigo woeid

--- src/app/weather-forecast/weather-forecast.service.ts
+++ src/app/weather-forecast/weather-forecast.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherForecastService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient,
+    private httpParams: HttpParams
+  ) { }
+
+  getKeyValidation(apiKey: string): Observable<any> {
+    let params = this.httpParams.set('key', apiKey);
+    return this.httpClient.get<any>(`https://hgbrasil.com/weather`, {params});
+  }
+
+  getWeatherByCityCode(apiKey:string, woeidCode: string): Observable<any> {
+    let params = this.httpParams;
+    params.set('key', apiKey);
+    params.set('woeid', woeidCode);
+    return this.httpClient.get<any>(`https://api.hgbrasil.com/weather?`, {params});
+  }
+
+  getWeatherByCityName(apiKey:string, cityName: string): Observable<any> {
+    let params = this.httpParams;
+    params.set('key', apiKey);
+    params.set('city_name', cityName);
+    return this.httpClient.get<any>(`https://api.hgbrasil.com/weather?`, {params});
+  }
 }
