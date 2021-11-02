340846561074f48d9e3d9dd6cf8e60282a3a3c05
Jsimondi <simondi.jorge@gmail.com>
2021-11-02 17:23:19 -0300

    Adicionado valor de chave para a API como variavel de ambiente

--- src/app/weather-forecast/weather-forecast.service.ts
+++ src/app/weather-forecast/weather-forecast.service.ts
@@ -9,25 +9,35 @@ export class WeatherForecastService {
 
   constructor(
     private httpClient: HttpClient,
-    private httpParams: HttpParams
   ) { }
 
+  options = {
+    headers: {
+      // 'x-rapidapi-host': 'omgvamp-hearthstone-v1.p.rapidapi.com',
+      'x-rapidapi-key': 'aaa1afff'
+    }
+  }
+
   getKeyValidation(apiKey: string): Observable<any> {
-    let params = this.httpParams.set('key', apiKey);
+    let params = new HttpParams()
+    params = params.set('key', apiKey);
+    params = params.set('format', 'json-cors')
     return this.httpClient.get<any>(`https://hgbrasil.com/weather`, {params});
   }
 
   getWeatherByCityCode(apiKey:string, woeidCode: string): Observable<any> {
-    let params = this.httpParams;
-    params.set('key', apiKey);
-    params.set('woeid', woeidCode);
-    return this.httpClient.get<any>(`https://api.hgbrasil.com/weather?`, {params});
+    let params = new HttpParams()
+    params = params.set('key', apiKey);
+    params = params.set('woeid', woeidCode);
+    params = params.set('format', 'json-cors')
+    return this.httpClient.get<any>(`https://api.hgbrasil.com/weather`, {params});
   }
 
   getWeatherByCityName(apiKey:string, cityName: string): Observable<any> {
-    let params = this.httpParams;
-    params.set('key', apiKey);
-    params.set('city_name', cityName);
-    return this.httpClient.get<any>(`https://api.hgbrasil.com/weather?`, {params});
+    let params = new HttpParams();
+    params = params.set('key', apiKey);
+    params = params.set('city_name', cityName);
+    params = params.set('format', 'json-cors')
+    return this.httpClient.get<any>(`https://api.hgbrasil.com/weather`, {params});
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // apiKey: 'b02d205a'
+  apiKey: 'aaa1afff'
 };
 
 /*
