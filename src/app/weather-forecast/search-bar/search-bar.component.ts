3894e6c8e707ba4a06527a7467b199a14c40d3f0
Jsimondi <simondi.jorge@gmail.com>
2021-11-02 17:55:18 -0300

    Criado formulario para a barra de busca e o metodo que ira emitir este valor para os componentes que utilizarem esta barra

--- src/app/weather-forecast/search-bar/search-bar.component.ts
+++ src/app/weather-forecast/search-bar/search-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent implements OnInit {
+  @Output() searchValueEmitter = new EventEmitter;
+  searchBarForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) { 
+    this.searchBarForm = this.formBuilder.group({
+      search: ['', Validators.required]
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  emitSearchValue() {
+    this.searchValueEmitter.emit(this.searchBarForm.value);
   }
 
 }
