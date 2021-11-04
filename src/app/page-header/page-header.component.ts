1084d4f4913fbb6e5e8ace9414e89d971ce27959
Jsimondi <simondi.jorge@gmail.com>
2021-11-04 02:14:30 -0300

    Novo formato de navegacao entre as paginas do header, agora ao alterar uma pagina o seletor ira mudar tambem. Ainda falta pegar o valor do inicio e fazer o seletor atualizar.

--- src/app/page-header/page-header.component.ts
+++ src/app/page-header/page-header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-header',
@@ -8,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 export class PageHeaderComponent implements OnInit {
   selectedNav = 'inicio'
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    console.log('Activated Route: ', this.activatedRoute);
+  }
+
+  switchSelectedNav(route: string) {
+    switch(route) {
+      case 'inicio':
+        this.selectedNav = 'inicio';
+        break;
+      case 'woei':
+        this.selectedNav = 'woei';
+        break;
+      case 'sobre':
+        this.selectedNav = 'sobre';
+        break;
+    } 
+  }
+
+  navigate(route: string) {
+    this.switchSelectedNav(route)
+    this.router.navigate([route])
   }
 
 }
